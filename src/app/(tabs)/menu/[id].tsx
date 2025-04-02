a2dce8d62494619fdfc6c3d0fd8aeaa0a06fede5
Diego Fernández Criado <contact@diegofercri.dev>
2025-04-02 15:02:26 +0200

    add size selector

--- src/app/(tabs)/menu/[id].tsx
+++ src/app/(tabs)/menu/[id].tsx
@@ -1,13 +1,16 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, Pressable } from "react-native";
 import { Stack } from "expo-router";
 import React from "react";
 import { useLocalSearchParams } from "expo-router";
 import products from "@assets/data/products";
+import { useState } from "react";
 
-const sizes = ['S', 'M', 'L', 'XL'];
+const sizes = ["S", "M", "L", "XL"];
 
 const ProductDetailsScreen = () => {
-  const  {id} = useLocalSearchParams();
+  const { id } = useLocalSearchParams();
+
+  const [selectedSize, setSelectedSize] = useState("M");
 
   const product = products.find((p) => p.id.toString() === id);
 
@@ -20,6 +23,30 @@ const ProductDetailsScreen = () => {
     <View style={styles.container}>
       <Stack.Screen options={{ title: product?.name }} />
       <Image source={{ uri: product.image }} style={styles.image} />
+      <Text style={{ fontWeight: "bold" }}>Select Size</Text>
+      <View style={styles.sizeContainer}>
+        {sizes.map((size) => (
+          <Pressable
+            onPress={() => setSelectedSize(size)}
+            style={[
+              styles.size,
+              { backgroundColor: selectedSize === size ? "#5454ff8f" : "#eee" },
+            ]}
+            key={size}
+            onTouchEnd={() => setSelectedSize(size)}
+          >
+            <Text
+              style={[
+                { color: selectedSize === size ? "#fff" : "#000" },
+                { fontWeight: selectedSize === size ? "bold" : "normal" },
+              ]}
+              key={size}
+            >
+              {size}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
       <Text style={styles.price}>{product.price}€</Text>
     </View>
   );
@@ -28,11 +55,31 @@ const ProductDetailsScreen = () => {
 export default ProductDetailsScreen;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    padding: 20,
+    gap: 20,
+  },
   image: {
     width: "100%",
     aspectRatio: 1,
     resizeMode: "contain",
   },
-  price: {},
+  price: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  sizeContainer: {
+    flexDirection: "row",
+    gap: 10,
+  },
+  size: {
+    backgroundColor: "#5454ff8f",
+    height: 50,
+    width: 50,
+    borderRadius: 100,
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
